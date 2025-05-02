1d30fb810f8a09beb865cbfdabfadab19788600d
erkutilaslan <erkutilaslan@gmail.com>
2025-05-02 14:43:14 +0200

    fix replace_srx_to_sample.sh

--- replace_srx_to_sample.sh
+++ replace_srx_to_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # file containing SRR to sample name mappings
-table_file="srx_sample_name.csv"
+table_file="srx_sample_name_v0.2.2.csv"
 
 # loop through each line in the table file
 while IFS=',' read -r srx_name sample_name; do
@@ -9,8 +9,9 @@ while IFS=',' read -r srx_name sample_name; do
     # find and rename R1 fastq.gz files
     find . -type f -name "${srx_name}_*_1.fastq.gz" | while read -r file; do
 
+        file_dir=$(dirname "$file")
         srx_srr=$(basename "$file")
-        mv $file ${sample_name}.${srx_srr}
+        mv $file ${file_dir}/${sample_name}.${srx_srr}
         echo "Renamed $file to ${sample_name}.${srx_srr}"
 
     done
@@ -18,8 +19,19 @@ while IFS=',' read -r srx_name sample_name; do
     # find and rename R2 fastq.gz files if they exist
     find . -type f -name "${srx_name}_*_2.fastq.gz" | while read -r file; do
 
+        file_dir=$(dirname "$file")
         srx_srr=$(basename "$file")
-        mv $file ${sample_name}.${srx_srr}
+        mv $file ${file_dir}/${sample_name}.${srx_srr}
+        echo "Renamed $file to ${sample_name}.${srx_srr}"
+
+    done
+
+    # find and rename non-paired files if they exist
+    find . -type f -name "${srx_name}*.fastq.gz" | while read -r file; do
+
+        file_dir=$(dirname "$file")
+        srx_srr=$(basename "$file")
+        mv $file ${file_dir}/${sample_name}.${srx_srr}
         echo "Renamed $file to ${sample_name}.${srx_srr}"
 
     done
